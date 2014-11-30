12a94ee47ca4e14bbbf91a6f6936d4f6ac1f2ab0
Alex Jackson <alex@alexj.org>
2014-11-30 19:00:02 +0000

    Added julia script to integrate functions using the Romberg method
    It's currently slow, taking unreasonably long times for integration levels greater than 20. Will fix.

--- Julia/Integration/romberg.jl
+++ Julia/Integration/romberg.jl
@@ -1,3 +1,32 @@
+function romberg(f::Function, a::Number, b::Number, intLevel::Int)
+  #=
+  Keyword arguments:
+  f -- Anonymous functon to integrate
+  a -- Lower limit of integration
+  b -- Upper limit of integration
+  intLevel -- The level of integration to go to.
+
+  Returns:
+  The result of the integration of f between a & b
+  =#
+
+  intTable = zeros(intLevel, intLevel);
+
+  h = (b - a);
+  for k = 1:intLevel
+    intTable[k,1] = 0.5 * h * (f(a) + 2*sum(f([a+h:h:b-h])) + f(b));
+    h /= 2;
+  end
+
+  for k = 2:intLevel
+    for j = size(intTable)[1]-k+1:-1:1
+      intTable[j, k] = ((4^(k-1))intTable[j+1, k-1] - intTable[j, k-1])/((4^(k-1)) - 1);
+    end
+  end
+
+  return intTable[1, end];
+end
+
 function romberg(intEstimates::Array{Float64,1})
   #= Estiamte integrals from sets of data using the Romberg Method.
 
