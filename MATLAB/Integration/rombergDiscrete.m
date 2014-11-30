fc815a169743f00b8ff18f54fb1891b8d30379d7
Alex Jackson <alex@alexj.org>
2014-11-30 19:52:44 +0000

    Commented thte rombergDiscrete MATLAB function

--- MATLAB/Integration/rombergDiscrete.m
+++ MATLAB/Integration/rombergDiscrete.m
@@ -14,17 +14,23 @@ function [I, rErr] = rombergDiscrete(intEstimates)
     intEstimates = intEstimates';
   end
 
+  % Empty matrix and vector for the divided difference table and the
+  % relative error on each step.
   intTable = zeros(length(intEstimates));
   relativeErrorTable = zeros(length(intTable), 1);
-
+ 
+  % Initialise the first column of the div diff table.
   intTable(:, 1) = intEstimates;
 
+  % Calculate the remaining elements of the div diff table.
   for kk = 2:length(intTable)
     for jj = length(intTable)-kk+1:-1:1
       intTable(jj, kk) = ((4^(kk-1))*intTable(jj+1, kk-1) - intTable(jj, kk-1)) / (4^(kk-1) - 1);
     end
   end
 
+  % Calculate the relative error between the two least accurate integrals
+  % at each step of Romberg's method.
   for kk = 2:length(intTable)
     relativeErrorTable(kk, 1) = 100 * abs((intTable(1, kk) - intTable(2, kk-1)) / intTable(1, kk));
   end
