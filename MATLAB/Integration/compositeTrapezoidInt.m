f57f2e62f880823f975459728a0ef606d6260ab2
Alex Jackson <alex@alexj.org>
2014-12-01 19:21:58 +0000

    Altered compositeTrapezoidInt MATLAB funciton
    Now the parameter n is the number of sections to use rather than the number of data points

--- MATLAB/Integration/compositeTrapezoidInt.m
+++ MATLAB/Integration/compositeTrapezoidInt.m
@@ -1,12 +1,12 @@
-function [I] = compositeTrapezoidInt(f, x1, xn, n)
+function [I] = compositeTrapezoidInt(f, a, b, n)
 %compositeTrapezoidInt Integrate functions using the composite trapezoid rule
-%   I=compositeTrapezoidInt(f,x1,xn,n) estimates the integral of the
-%   function f between x1 through xn using n datapoints with the composite
+%   I=compositeTrapezoidInt(f,a,b,n) estimates the integral of the
+%   function f between a through b using n sections with the composite
 %   trapezoid rule. Note that f must be vectorized. 
-  if n <= 1
-    error('Insufficent data points. Try increasing n.');
+  if n <= 0 || mod(n, 1) ~= 0
+    error('n must be a positive integer.');
   end
   
-  h = (xn-x1) / (n - 1);
-  I = 0.5 * h * (f(x1) + 2*sum(f(x1+h:h:xn-h)) + f(xn));
+  h = (b-a) / n;
+  I = 0.5 * h * (f(a) + 2*sum(f(a+h:h:b-h)) + f(b));
 end
