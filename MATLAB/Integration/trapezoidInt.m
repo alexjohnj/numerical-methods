49824f9c39b085c51f07bbd2fefb1c00fc186f02
Alex Jackson <alex@alexj.org>
2014-12-01 19:24:06 +0000

    Removed trapezoidInt function and renamed composite MATLAB function

--- MATLAB/Integration/trapezoidInt.m
+++
@@ -1,6 +0,0 @@
-function [I] = trapezoidInt(f, x1, x2)
-%trapezoidInt Integrate functions using the trapezoid rule
-%   I=trapezoidInt(f,x1,x2) estimates the integral I of the function f
-%   between the limits x1 and x2 using a single trapezoid.
-  I = (x2 - x1) * 0.5 * (f(x1) + f(x2));
-end
--- MATLAB/Integration/trapz.m
+++ MATLAB/Integration/trapz.m
@@ -1,6 +1,6 @@
-function [I] = compositeTrapezoidInt(f, a, b, n)
-%compositeTrapezoidInt Integrate functions using the composite trapezoid rule
-%   I=compositeTrapezoidInt(f,a,b,n) estimates the integral of the
+function [I] = trapz(f, a, b, n)
+%TRAPZ Integrate functions using the composite trapezoid rule
+%   I=trapz(f,a,b,n) estimates the integral of the
 %   function f between a through b using n sections with the composite
 %   trapezoid rule. Note that f must be vectorized. 
   if n <= 0 || mod(n, 1) ~= 0
