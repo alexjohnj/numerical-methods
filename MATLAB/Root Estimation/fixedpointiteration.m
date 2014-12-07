ebbade5e591483f5b6b1e91160818d9c199cccc4
Alex Jackson <alex@alexj.org>
2014-12-07 21:32:16 +0000

    Improved fixedpointiteration MATLAB function
    It actually, you know, works now

--- MATLAB/Root Estimation/fixedpointiteration.m
+++ MATLAB/Root Estimation/fixedpointiteration.m
@@ -1,16 +1,14 @@
-function [xr] = fixedpointiteration(g, xi, ea, maxIter)
+function [xi] = fixedpointiteration(g, xi, ea, maxIter)
 %fixedpointiteration Estimate roots using the fixed point iteration method
 %   xr = fixedpointiteration(g,xi,ea,maxIter) estimates the root xr of a
 %   function using the rearrangement g of the function in terms of x. Using
 %   xi as an initial estimate, it iterates on the root until the relative
-%   error falls below ea or maxIter iterations occur. 
-  xr = xi;
-  
+%   error falls below ea or maxIter iterations occur.   
   for ii = 1:maxIter
     xiold = xi;
     xi = g(xi);
     
-    if (abs(xi - xiold) / xi) < ea
+    if (abs(xi - xiold) / xi) < ea && ii > 1
       break;
     end
   end
